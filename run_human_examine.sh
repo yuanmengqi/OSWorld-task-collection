ea60fca66f438d2ae29e063be12e0b2089046c89
yuanmengqi <yuanmengqi@mail.ustc.edu.cn>
2025-10-18 07:49:02 +0000

    add manual examine code

---
+++ run_human_examine.sh
@@ -0,0 +1,9 @@
+python manual_examine.py \
+     --headless \
+     --observation_type screenshot \
+     --result_dir ./results_new_tasks \
+     --test_all_meta_path evaluation_examples/test_new.json \
+     --region us-east-1 \
+     --domain new_tasks \
+     --example_id 'example' \
+     --max_steps 3
